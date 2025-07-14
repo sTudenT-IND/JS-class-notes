94596bfcdaf714f7e7f768fd5dc9e093ab647505
sTudenT-IND <infotnp2025@gmail.com>
2025-07-14 14:35:29 +0530

    add

--- type_conversion.js
+++ type_conversion.js
@@ -1,12 +1,29 @@
 let score = 1;
-console.log(typeof score);
+console.log(typeof score);                                  //number
+
+
  let boolVal = Boolean(score)
- console.log(typeof boolVal);
- console.log(boolVal);            //real val
+ console.log(typeof boolVal);                               //boolean
+ console.log(boolVal);            //real val                // true
+
+
   let stringVal = String(boolVal)
-  console.log(typeof stringVal);
-  console.log(stringVal);        //real val
+  console.log(typeof stringVal);                            //string
+  console.log(stringVal);        //real val                 //true
   
   
+ let score1 = ""
+ console.log(typeof score1);
+ let newval = Number(score1)
+ console.log(typeof newval);
+ console.log(newval);
+  
+
+ let score2 = " abhay "
+console.log(typeof score2);
+ let newval1 = Number(score2)
+ console.log(typeof newval1);
+ console.log(newval1);
+ 
  
  
